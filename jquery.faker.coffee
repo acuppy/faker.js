bbea13b0565fc6ff8fec66dc01f3fba66b347c4a
acuppy <acuppy@gmail.com>
2013-06-27 21:16:26 -0700

    coffeescript

---
+++ jquery.faker.coffee
@@ -0,0 +1,210 @@
+  $ = jQuery
+
+  Faker = {}
+
+  Faker.Base =
+    config: {}
+    reset: ->
+      @configure {}
+      return
+
+    configure: (configs) ->
+      $.error "Hash expected -- received #{$.type(configs)}" unless $.isPlainObject(configs)
+
+      baseConfig =
+        locale: 'en'
+
+      @config = $.extend(true, {}, baseConfig, configs)
+      return
+
+    extend: (extension) ->
+      $.extend(true, {}, this, extension || {})
+
+  # rebase the Faker object on the Base object
+  Faker = $.extend(true, {}, Faker.Base)
+  # alias the init method to reset for now
+  Faker.init = Faker.reset
+
+  Faker.Lorem = Faker.extend
+    word: ->
+      Faker.Locale.sample("lorem.words")[0]
+
+    words: (qty) ->
+      $.error "Quantity must be a valid integer" unless $.isNumeric qty
+      Faker.Locale.sample "lorem.words", qty
+
+    characters: (qty, as_array) ->
+      $.error("Quantity must be a valid integer") unless $.isNumeric(qty)
+      arr = Faker.Locale.sample("lorem.characters", qty)
+      if as_array == true then arr else arr.join('')
+
+    sentence: (options) ->
+      options = { length: options } if $.isNumeric(options)
+
+      opt = $.extend({
+        length: 10
+        startWithCapital: true
+        endWithPeriod: true
+      }, options)
+
+      arr    = @words opt.length
+      arr[0] = "#{arr[0].charAt(0).toUpperCase()}#{arr[0].slice(1)}" if opt.startWithCapital == true
+
+      sentence = arr.join ' '
+      sentence += '.' if opt.endWithPeriod == true
+      sentence
+
+    sentences: (qty) ->
+      $.error "Quantity must be a valid integer" unless $.isNumeric qty
+
+      args      = Array.prototype.slice.call arguments, 1
+      options   = {}
+      as_array  = false
+      sentences = new Array()
+
+      if args.length > 0 and $.type(args[args.length - 1]) == 'boolean'  then as_array = args[args.length - 1]
+      if $.isArray(args) and args.length > 0 and $.isPlainObject args[0] then $.extend options, args[0]
+
+      for i in [0...qty]
+        sentences.push @sentence options
+
+      if as_array == true then sentences else sentences.join ' ' 
+
+    paragraph: (sentences, options) ->
+      sentences ?= 3
+      options   ?= {}
+
+      $.error "First argument must be a valid integer" unless $.isNumeric sentences
+
+      @sentences sentences, options
+
+    paragraphs: (qty) ->
+      $.error "Quantity must be a valid integer" unless $.isNumeric qty
+
+      args      = Array.prototype.slice.call arguments, 1
+      options   = {}
+      as_array  = false
+      paragraphs = new Array()
+
+      if args.length > 0 and $.type(args[args.length - 1]) == 'boolean' then as_array = args[args.length - 1]  
+      if $.isArray args and args.length > 0 and $.isPlainObject args[0] then $.extend options, args[0]
+
+      for i in [0...qty]
+        paragraphs.push @paragraph 3, options
+
+      if as_array == true then paragraphs else paragraphs.join(' ')
+
+  Faker.Name = Faker.extend
+    full_name: ->
+      name = new Array();
+      name.push this.prefix() if Faker.Util.Random.bool()
+      name.push this.first_name()
+      name.push this.last_name()
+      name.push this.suffix() if Faker.Util.Random.bool()
+      name.join(' ')
+
+    prefix: ->
+      Faker.Locale.sample('name.prefix')
+
+    first_name: ->
+      Faker.Locale.sample('name.first_name')
+
+    last_name: ->
+      Faker.Locale.sample('name.last_name')
+
+    suffix: ->
+      Faker.Locale.sample('name.suffix')
+
+    title: ->
+      title = new Array()
+      title.push Faker.Locale.sample('name.title.descriptor')
+      title.push Faker.Locale.sample('name.title.level')
+      title.push Faker.Locale.sample('name.title.job')
+      title.join(' ')
+
+  Faker.Company = Faker.extend
+    name: ->
+      Faker.Util.interpret Faker.Locale.sample("company.name")[0]
+
+    suffix: ->
+      Faker.Locale.sample("company.suffix")[0]
+
+    catch_phrase: ->
+      $.map(Faker.Locale.collection("company.buzzwords"), (elm, i) ->
+        Faker.Util.Random.sample(elm)
+      ).join(' ')
+
+    bs: ->
+      $.map(Faker.Locale.collection("company.bs"), (elm, i) ->
+        Faker.Util.Random.sample(elm)
+      ).join(' ')
+
+  Faker.Util = 
+    isBlank: (object) ->
+      ($.isPlainObject(object) and $.isEmptyObject(object)) or
+      ($.isArray(object) and object.length == 0) or
+      ($.type(object) == "string" and $.trim(object) == "") or
+      (!object)
+
+    isPresent: ->
+     !this.isBlank.apply(this, arguments)
+
+    interpret: (raw) ->
+      blocksToInterpret = raw.match(/#{([a-zA-Z\.\-\_]+)}/gi);
+      
+      $.each(blocksToInterpret, (index, elm) ->
+        key = elm.replace(/[#{}\s]/gi, '')
+        # "lorem.name" not "{lorem.name}" or "lorem.name."
+        raw = raw.replace elm, Faker.Locale.sample(key) if key.match(/^(?:(?:[a-zA-Z\-\_]+)\.?)+[a-zA-Z]+$/)
+      )
+      raw
+
+  Faker.Util.Random = 
+    bool: ->
+      (Math.floor(Math.random()*11) % 2) == 1
+
+    sample: (collection, size) ->
+      shuffled = collection.slice(0)
+      i = collection.length
+
+      while i--
+        index = Math.floor(i * Math.random())
+        temp  = shuffled[index]
+        shuffled[index] = shuffled[i]
+        shuffled[i] = temp
+
+      shuffled.slice(0, size or 1)
+
+  Faker.Locale = 
+    sample: (key, size) ->
+      collection = this.collection(key)
+      Faker.Util.Random.sample(collection, size)
+
+    collection: (key) ->
+
+      collection = Faker.Locales[Faker.config.locale]
+      arrPath    = key.split(".")
+
+      for i in [0...arrPath.length]
+        collection = collection[arrPath[i]]
+
+      $.error "Unknown key: \"#{key.toString()}\" -- you may need to scope it to the proper locale library (ex: name.first_name)" if Faker.Util.isBlank(collection)
+
+      collection
+
+    register: (id, collection) ->
+      Faker.Locales[id] = collection
+      return
+
+    extend: ->
+      Faker.Locales[id] ?= {}
+      $.extend(true, Faker.Locales[id], collection)
+
+  Faker.Locales = {}
+  Faker.init()
+
+  # bind it to the global namespace
+  window.Faker = Faker
+
+  $.fn.faker = ->
+    this.each ->
--- jquery.faker.js
+++ jquery.faker.js
@@ -1,183 +1,172 @@
-(function($, window){
+(function() {
+  var $, Faker;
 
-  window.Faker = {}
+  $ = jQuery;
+
+  Faker = {};
 
   Faker.Base = {
     config: {},
-
-    reset: function(){
+    reset: function() {
       this.configure({});
     },
-
-    configure: function(configs){
-      if ( ! $.isPlainObject(configs) ) $.error("Hash expected -- received "+ $.type(configs));
-
-      var baseConfig = {
-        locale: 'en'
+    configure: function(configs) {
+      var baseConfig;
+      if (!$.isPlainObject(configs)) {
+        $.error("Hash expected -- received " + ($.type(configs)));
       }
-
+      baseConfig = {
+        locale: 'en'
+      };
       this.config = $.extend(true, {}, baseConfig, configs);
     },
-
-    extend: function(extension){
+    extend: function(extension) {
       return $.extend(true, {}, this, extension || {});
     }
-  }
+  };
 
-  // rebase the Faker object on the Base object
   Faker = $.extend(true, {}, Faker.Base);
-  // alias the init method to reset for now
+
   Faker.init = Faker.reset;
 
   Faker.Lorem = Faker.extend({
-    word: function(){
+    word: function() {
       return Faker.Locale.sample("lorem.words")[0];
     },
-
-    words: function(qty){
-      if( ! $.isNumeric(qty) ) $.error("Quantity must be a valid integer");
+    words: function(qty) {
+      if (!$.isNumeric(qty)) $.error("Quantity must be a valid integer");
       return Faker.Locale.sample("lorem.words", qty);
     },
-
-    characters: function(qty, as_array){
-      if( ! $.isNumeric(qty) ) $.error("Quantity must be a valid integer");
-      var arr = Faker.Locale.sample("lorem.characters", qty);
-      return as_array === true ? arr : arr.join('');
-    },
-
-    sentence: function(options){
-
-      if( $.isNumeric(options) ){
-        options = { length: options }
+    characters: function(qty, as_array) {
+      var arr;
+      if (!$.isNumeric(qty)) $.error("Quantity must be a valid integer");
+      arr = Faker.Locale.sample("lorem.characters", qty);
+      if (as_array === true) {
+        return arr;
+      } else {
+        return arr.join('');
       }
-
-      var opt = $.extend({
+    },
+    sentence: function(options) {
+      var arr, opt, sentence;
+      if ($.isNumeric(options)) {
+        options = {
+          length: options
+        };
+      }
+      opt = $.extend({
         length: 10,
         startWithCapital: true,
         endWithPeriod: true
       }, options);
-
-      var arr = this.words(opt.length);
-      if(opt.startWithCapital === true) arr[0] = arr[0].charAt(0).toUpperCase() + arr[0].slice(1);
-
-      var sentence = arr.join(' ');
-      if(opt.endWithPeriod === true) sentence += '.'
-
+      arr = this.words(opt.length);
+      if (opt.startWithCapital === true) {
+        arr[0] = "" + (arr[0].charAt(0).toUpperCase()) + (arr[0].slice(1));
+      }
+      sentence = arr.join(' ');
+      if (opt.endWithPeriod === true) sentence += '.';
       return sentence;
     },
-
-    sentences: function(qty){
-      if( ! $.isNumeric(qty) ) $.error("Quantity must be a valid integer");
-
-      var args      = Array.prototype.slice.call(arguments, 1);
-      var options   = {};
-      var as_array  = false;
-      var sentences = new Array();
-
-      if( args.length > 0 && typeof args[args.length - 1] == 'boolean') 
+    sentences: function(qty) {
+      var args, as_array, i, options, sentences;
+      if (!$.isNumeric(qty)) $.error("Quantity must be a valid integer");
+      args = Array.prototype.slice.call(arguments, 1);
+      options = {};
+      as_array = false;
+      sentences = new Array();
+      if (args.length > 0 && $.type(args[args.length - 1]) === 'boolean') {
         as_array = args[args.length - 1];
-
-      if( $.isArray(args) && args.length > 0 && $.isPlainObject(args[0]) ) 
+      }
+      if ($.isArray(args) && args.length > 0 && $.isPlainObject(args[0])) {
         $.extend(options, args[0]);
-
-      for (var i = 0; i < qty; i++) {
+      }
+      for (i = 0; 0 <= qty ? i < qty : i > qty; 0 <= qty ? i++ : i--) {
         sentences.push(this.sentence(options));
-      };
-
-      return as_array === true ? sentences : sentences.join(' ') 
+      }
+      if (as_array === true) {
+        return sentences;
+      } else {
+        return sentences.join(' ');
+      }
     },
-
-    paragraph: function(sentences, options){
-      var sentences = sentences || 3;
-      var options   = options   || {};
-
-      if( ! $.isNumeric(sentences) ) $.error("First argument must be a valid integer");
-
+    paragraph: function(sentences, options) {
+      if (sentences == null) sentences = 3;
+      if (options == null) options = {};
+      if (!$.isNumeric(sentences)) {
+        $.error("First argument must be a valid integer");
+      }
       return this.sentences(sentences, options);
     },
-
-    paragraphs: function(qty){
-      if( ! $.isNumeric(qty) ) $.error("Quantity must be a valid integer");
-
-      var args      = Array.prototype.slice.call(arguments, 1);
-      var options   = {};
-      var as_array  = false;
-      var paragraphs = new Array();
-
-      if( args.length > 0 && typeof args[args.length - 1] == 'boolean') 
+    paragraphs: function(qty) {
+      var args, as_array, i, options, paragraphs;
+      if (!$.isNumeric(qty)) $.error("Quantity must be a valid integer");
+      args = Array.prototype.slice.call(arguments, 1);
+      options = {};
+      as_array = false;
+      paragraphs = new Array();
+      if (args.length > 0 && $.type(args[args.length - 1]) === 'boolean') {
         as_array = args[args.length - 1];
-
-      if( $.isArray(args) && args.length > 0 && $.isPlainObject(args[0]) ) 
+      }
+      if ($.isArray(args && args.length > 0 && $.isPlainObject(args[0]))) {
         $.extend(options, args[0]);
-
-      for (var i = 0; i < qty; i++) {
+      }
+      for (i = 0; 0 <= qty ? i < qty : i > qty; 0 <= qty ? i++ : i--) {
         paragraphs.push(this.paragraph(3, options));
-      };
-
-      return as_array === true ? paragraphs : paragraphs.join(' ');
+      }
+      if (as_array === true) {
+        return paragraphs;
+      } else {
+        return paragraphs.join(' ');
+      }
     }
   });
 
   Faker.Name = Faker.extend({
-    full_name: function(){
-      var name = new Array();
-      
-      if( Faker.Util.Random.bool() ){
-        name.push(this.prefix());
-      }
-      
+    full_name: function() {
+      var name;
+      name = new Array();
+      if (Faker.Util.Random.bool()) name.push(this.prefix());
       name.push(this.first_name());
       name.push(this.last_name());
-      
-      if( Faker.Util.Random.bool() ){
-        name.push(this.suffix());
-      }
-
+      if (Faker.Util.Random.bool()) name.push(this.suffix());
       return name.join(' ');
     },
-
-    prefix: function(){
+    prefix: function() {
       return Faker.Locale.sample('name.prefix');
     },
-
-    first_name: function(){
+    first_name: function() {
       return Faker.Locale.sample('name.first_name');
     },
-
-    last_name: function(){
+    last_name: function() {
       return Faker.Locale.sample('name.last_name');
     },
-
-    suffix: function(){
+    suffix: function() {
       return Faker.Locale.sample('name.suffix');
     },
-
-    title: function(){
-      var title = new Array();
+    title: function() {
+      var title;
+      title = new Array();
       title.push(Faker.Locale.sample('name.title.descriptor'));
       title.push(Faker.Locale.sample('name.title.level'));
       title.push(Faker.Locale.sample('name.title.job'));
       return title.join(' ');
-    }   
+    }
   });
 
   Faker.Company = Faker.extend({
-    name: function(){
+    name: function() {
       return Faker.Util.interpret(Faker.Locale.sample("company.name")[0]);
     },
-
-    suffix: function(){
+    suffix: function() {
       return Faker.Locale.sample("company.suffix")[0];
     },
-
-    catch_phrase: function(){
-      return $.map(Faker.Locale.collection("company.buzzwords"), function(elm, i){
+    catch_phrase: function() {
+      return $.map(Faker.Locale.collection("company.buzzwords"), function(elm, i) {
         return Faker.Util.Random.sample(elm);
       }).join(' ');
     },
-
-    bs: function(){
-      return $.map(Faker.Locale.collection("company.bs"), function(elm, i){
+    bs: function() {
+      return $.map(Faker.Locale.collection("company.bs"), function(elm, i) {
         return Faker.Util.Random.sample(elm);
       }).join(' ');
     }
@@ -185,83 +174,68 @@
 
   Faker.Util = {
     isBlank: function(object) {
-      return (
-        ($.isPlainObject(object) && $.isEmptyObject(object)) ||
-        ($.isArray(object) && object.length == 0) ||
-        (typeof(object) == "string" && $.trim(object) === "") ||
-        (!object)
-      );
+      return ($.isPlainObject(object) && $.isEmptyObject(object)) || ($.isArray(object) && object.length === 0) || ($.type(object) === "string" && $.trim(object) === "") || (!object);
     },
-
-    isPresent: function(){
-     return !this.isBlank.apply(this, arguments);
+    isPresent: function() {
+      return !this.isBlank.apply(this, arguments);
     },
-
-    interpret: function(raw){
-      var blocksToInterpret = raw.match(/#{([a-zA-Z\.\-\_]+)}/gi);
-      
-      $.each(blocksToInterpret, function(index, elm){
-        var key = elm.replace(/[#{}\s]/gi, '');
-        // "lorem.name" not "{lorem.name}" or "lorem.name."
-        if( key.match(/^(?:(?:[a-zA-Z\-\_]+)\.?)+[a-zA-Z]+$/) ){
-          raw = raw.replace(elm, Faker.Locale.sample(key));
+    interpret: function(raw) {
+      var blocksToInterpret;
+      blocksToInterpret = raw.match(/#{([a-zA-Z\.\-\_]+)}/gi);
+      $.each(blocksToInterpret, function(index, elm) {
+        var key;
+        key = elm.replace(/[#{}\s]/gi, '');
+        if (key.match(/^(?:(?:[a-zA-Z\-\_]+)\.?)+[a-zA-Z]+$/)) {
+          return raw = raw.replace(elm, Faker.Locale.sample(key));
         }
       });
-
       return raw;
     }
   };
 
   Faker.Util.Random = {
-    bool: function(){
-      return (Math.floor(Math.random()*11) % 2) == 1
+    bool: function() {
+      return (Math.floor(Math.random() * 11) % 2) === 1;
     },
-
-    sample: function(collection, size){
-      var shuffled = collection.slice(0);
-      var i = collection.length;
-      var temp;
-      var index;
-
+    sample: function(collection, size) {
+      var i, index, shuffled, temp;
+      shuffled = collection.slice(0);
+      i = collection.length;
       while (i--) {
         index = Math.floor(i * Math.random());
         temp = shuffled[index];
         shuffled[index] = shuffled[i];
         shuffled[i] = temp;
       }
-
       return shuffled.slice(0, size || 1);
     }
-  }
+  };
 
   Faker.Locale = {
-
-    sample: function(key, size){
-      var collection = this.collection(key);
+    sample: function(key, size) {
+      var collection;
+      collection = this.collection(key);
       return Faker.Util.Random.sample(collection, size);
     },
-
-    collection: function(key){
-
-      var collection = Faker.Locales[Faker.config.locale];
-      var arrPath = key.split(".");
-
-      for (var i = 0; i < arrPath.length; i++) {
+    collection: function(key) {
+      var arrPath, collection, i, _ref;
+      collection = Faker.Locales[Faker.config.locale];
+      arrPath = key.split(".");
+      for (i = 0, _ref = arrPath.length; 0 <= _ref ? i < _ref : i > _ref; 0 <= _ref ? i++ : i--) {
         collection = collection[arrPath[i]];
-      };
-
-      if( Faker.Util.isBlank(collection) ) $.error('Unknown key: "'+ key.toString() +'" -- you may need to scope it to the proper locale library (ex: name.first_name)');
-
+      }
+      if (Faker.Util.isBlank(collection)) {
+        $.error("Unknown key: \"" + (key.toString()) + "\" -- you may need to scope it to the proper locale library (ex: name.first_name)");
+      }
       return collection;
     },
-
-    register: function(id, collection){
+    register: function(id, collection) {
       Faker.Locales[id] = collection;
     },
-
-    extend: function(){
-      if( ! Faker.Locales[id] ) Faker.Locales[id] = {};
-      $.extend(true, Faker.Locales[id], collection);
+    extend: function() {
+      var _base;
+      if ((_base = Faker.Locales)[id] == null) _base[id] = {};
+      return $.extend(true, Faker.Locales[id], collection);
     }
   };
 
@@ -269,10 +243,10 @@
 
   Faker.init();
 
-  $.fn.faker = function(){
-    return this.each(function(){
-      
-    });
+  window.Faker = Faker;
+
+  $.fn.faker = function() {
+    return this.each(function() {});
   };
 
-})(jQuery, window);
+}).call(this);
